29b6c76db353949ef92e7f488c0a4d50e8cabfa6
Hh440 <harsh100045@gmail.com>
2025-05-17 11:40:36 +0530

    homechange

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
          <Route path='/simulation' element={<Simulaltion/>}/>
          <Route path='/game' element={<Game/>}/>
       </Routes>
